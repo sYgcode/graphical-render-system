b229cf2c74a4c5bb686d0453ee3acd874510a30b
shuag <shuagolubtchik@gmail.com>
2024-01-08 18:10:37 +0200

    unittests- started

--- unittests/primitives/PointTests.java
+++ unittests/primitives/PointTests.java
@@ -1,17 +1,21 @@
 package primitives;
 
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class PointTest {
+/**
+ * Unit tests for primitives.Point class
+ * @author Shua Golubtchik & Yair Yahav
+ */
+class PointTests {
 
     @Test
     void subtract() {
     }
-
+    /**
+     * Test method for {@link primitives.Point#add(primitives.Point)}.
+     */
     @Test
     void add() {
+
     }
 
     @Test
